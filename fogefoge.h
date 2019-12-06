c84f751d569ba19c45889781df38cbcce486dc55
Kleber Yoshida <kleber.yoshida@inmetrics.com.br>
2019-12-06 10:40:44 -0300

    primeiro commit

---
+++ fogefoge.h
@@ -0,0 +1,12 @@
+#define CIMA 'w'
+#define BAIXO 's'
+#define ESQUERDA 'a'
+#define DIREITA 'd'
+#define BOMBA 'b'
+
+void move(char direcao);
+int acabou();
+int ehdirecao(char direcao);
+void fantasmas();
+int praondefantasmavai(int xatual, int yatual, int* xdestino, int*ydestino);
+void explodepilula(int x, int y, int qtd);
---
+++ mapa.h
@@ -0,0 +1,32 @@
+#define HEROI '@'
+#define VAZIO '.'
+#define FANTASMA 'F'
+#define PAREDE_VERTICAL '|'
+#define PAREDE_HORIZONTAL '-'
+#define PILULA 'P'
+
+struct mapa{
+    char** matriz;
+    int linhas;
+    int colunas;
+};
+typedef struct mapa MAPA;
+
+struct posicao{
+    int x;
+    int y;
+};
+typedef struct posicao POSICAO;
+
+void liberamapa(MAPA* m);
+void lemapa(MAPA* m);
+void alocamapa(MAPA* m);
+void imprimi_mapa(MAPA* m);
+int encontramapa(MAPA* m, POSICAO* p, char c);
+int ehvalida(MAPA* m, int x, int y);
+int ehvazia(MAPA* m, int x, int y);
+void andanomapa(MAPA* m, int xorigem, int yorigem, int xdestino, int ydestino);
+void copiamapa(MAPA* destino, MAPA* origem);
+int podeandar(MAPA* m, char personagem, int x, int y);
+int ehparede(MAPA* m, int x, int y);
+int ehpersonagem(MAPA* m, char personagem, int x, int y);
